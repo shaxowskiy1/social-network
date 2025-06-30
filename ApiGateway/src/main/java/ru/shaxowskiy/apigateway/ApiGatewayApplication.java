256552d0ff55e608fe19219be8dd9bdd439f581f
Sergey <spavelin@inbox.ru>
2025-07-01 02:51:48 +0500

    add forwarding in api gateway to auth and post service

--- ApiGateway/src/main/java/ru/shaxowskiy/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/ru/shaxowskiy/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,8 @@ package ru.shaxowskiy.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
