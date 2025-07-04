a935f042fd95589ec5bd208bfc149ccc2046b7a1
Sergey <spavelin@inbox.ru>
2025-07-04 23:27:15 +0500

    first working prod-con connection

--- PostService/src/main/java/ru/shaxowskiy/socialservice/services/PostService.java
+++ PostService/src/main/java/ru/shaxowskiy/socialservice/services/PostService.java
@@ -1,5 +1,8 @@
 package ru.shaxowskiy.socialservice.services;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.shaxowskiy.socialservice.dto.PostCreateDTO;
@@ -7,13 +10,17 @@ import ru.shaxowskiy.socialservice.models.Post;
 import ru.shaxowskiy.socialservice.repository.PostRepository;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 
 @Service
+@Slf4j
 public class PostService {
     private final PostRepository postRepository;
+    private KafkaTemplate<String, Post> kafkaTemplate;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, KafkaTemplate<String, Post> kafkaTemplate) {
         this.postRepository = postRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Transactional
@@ -25,7 +32,21 @@ public class PostService {
                 .fileId(null)
                 .likes(null)
                 .comments(null).build();
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post);
+
+        CompletableFuture<SendResult<String, Post>> future =
+                kafkaTemplate.send("post-created-events-topic", savedPost.getId().toString(), post);
+
+
+        future.whenComplete((stringPostSendResult, throwable) -> {
+            if(throwable != null){
+                log.info("Failed to send message: {}", throwable.getMessage());
+            }
+            log.info("Message is successfull. Topic {}, Partition {}, Offset {}",
+                    stringPostSendResult.getRecordMetadata().topic(),
+                    stringPostSendResult.getRecordMetadata().partition(),
+                    stringPostSendResult.getRecordMetadata().offset());
+        });
         return post;
     }
 }
